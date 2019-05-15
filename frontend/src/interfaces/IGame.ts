b6c7ba05feb254ceab0fe29bfb8a90b6a4abbf9f
Sven Reber <sreber@edgelab.ch>
2019-05-16 00:39:37 +0200

    Add tests for games filtering service. #11

--- frontend/src/interfaces/IGame.ts
+++ frontend/src/interfaces/IGame.ts
@@ -1,26 +1,24 @@
-import {GameId, Date, LanguageId, BggId} from '@/interfaces/types';
+import {GameId, BggId} from '@/interfaces/types';
 
 export interface INonPersistedGame {
   bggId?: BggId;
-  buyDate?: Date;
-  extensions?: Array<GameId>;
-  isExtension?: boolean;
-  languages?: Array<LanguageId>;
   name?: string;
   playersCountMax?: number;
   playersCountMin?: number;
   playTimeMax?: number;
   playTimeMin?: number;
   urlTT?: string;
+
+  // To be added in later versions
+  // buyDate?: Date;
+  // extensions?: Array<GameId>;
+  // isExtension?: boolean;
+  // languages?: Array<LanguageId>;
 }
 
 export interface IGame extends INonPersistedGame {
   readonly bggId: BggId;
-  readonly buyDate: Date;
-  readonly extensions: Array<GameId>;
   readonly id: GameId;
-  readonly isExtension: boolean;
-  readonly languages: Array<LanguageId>;
   readonly name: string;
   readonly playersCountMax: number;
   readonly playersCountMin: number;
